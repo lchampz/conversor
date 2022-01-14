ebedc054c18f2eb6068560c6d4d67107c8880021
VictorLonchamps <victor.longchamps@pro22.com.br>
2022-01-14 08:44:42 -0300

    security changes on api request

--- src/components/logic/FuncConversor.js
+++ src/components/logic/FuncConversor.js
@@ -1,6 +1,7 @@
 export async function GetConversor(coinA, coinB) {
+    const api_key = process.env.API_KEY
     let de_para = `${coinA}_${coinB}`
-    const url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=d39289403a83a2b386ec`
+    const url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=${api_key}`
     const promise = await fetch(url)
     const data = await promise.json()
 
