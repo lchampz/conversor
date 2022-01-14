63ad5ef2af8d024e575e2157a25067aeabb778c1
VictorLonchamps <victor.longchamps@pro22.com.br>
2022-01-13 21:41:33 -0300

    correcting fetch verbosity

--- src/components/logic/FuncConversor.js
+++ src/components/logic/FuncConversor.js
@@ -1,12 +1,7 @@
-function fetchCurrency(coinA, coinB) {
-    let de_para = `${coinA}_${coinB}`
-    let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=d39289403a83a2b386ec`
-    return fetch(url)
-}
-
 export async function GetConversor(coinA, coinB) {
     let de_para = `${coinA}_${coinB}`
-    const promise = await fetchCurrency(coinA, coinB)
+    const url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=d39289403a83a2b386ec`
+    const promise = await fetch(url)
     const data = await promise.json()
 
     try{
